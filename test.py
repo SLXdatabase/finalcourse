6121658bd75e6aea97227a9e299b2a3e8c715dd3
mu001999 <mu001999@outlook.com>
2018-12-29 22:09:45 +0800

    update test.py

--- test.py
+++ test.py
@@ -6,26 +6,102 @@ from model import UserModel, ScoreModel, MovieModel
 import unittest
 
 
+def create_user_instance(
+        userid,
+        username,
+        password,
+        record
+    ):
+    user = UserModel()
+
+    user.userid = userid
+    user.username = username
+    user.password = password
+    user.record = record
+
+    user.save()
+    return user
+
+def create_score_instance(
+        userid,
+        movieid,
+        comDate,
+        value,
+        content,
+        watched,
+        star
+    ):
+    score = ScoreModel()
+
+    score.userid = userid
+    score.movieid = movieid
+    score.comDate = comDate
+    score.value = value
+    score.content = content
+    score.watched = watched
+    score.star = star
+
+    score.save()
+    return score
+
+def create_movie_instance(
+        movieid,
+        mname,
+        director,
+        editor,
+        actor,
+        mtype,
+        countryOrLocation,
+        language,
+        date,
+        duration,
+        othername,
+        IMDb,
+        introduction,
+        allActors,
+        numOfEvaluator,
+        star5,
+        star4,
+        star3,
+        star2,
+        star1
+    ):
+    movie = MovieModel()
+
+    movie.movieid = movieid
+    movie.mname = mname
+    movie.director = director
+    movie.editor = editor
+    movie.actor = actor
+    movie.mtype = mtype
+    movie.countryOrLocation = countryOrLocation
+    movie.language = language
+    movie.date = date
+    movie.duration = duration
+    movie.othername = othername
+    movie.IMDb = IMDb
+    movie.introduction = introduction
+    movie.allActors = allActors
+    movie.numOfEvaluator = numOfEvaluator
+    movie.star5 = star5
+    movie.star4 = star4
+    movie.star3 = star3
+    movie.star2 = star2
+    movie.star1 = star1
+
+    movie.save()
+    return movie
+
+
 class TestUserModel(unittest.TestCase):
 
-    def create_user_instance(
-            userid,
-            username,
-            password,
-            record
-        ):
-        user = UserModel()
-
-        user.userid = userid
-        user.username = username
-        user.password = password
-        user.record = record
-
-        user.save()
-        return user
-
     def test_instance(self):
-        pass
+        user = create_user_instance(
+            1314,
+            "lolo",
+            "bobo",
+            "520"
+        )
 
     def test_update(self):
         pass
@@ -41,28 +117,6 @@ class TestUserModel(unittest.TestCase):
 
 
 class TestScoreModel(unittest.TestCase):
-
-    def create_score_instance(
-            userid,
-            movieid,
-            comDate,
-            value,
-            content,
-            watched,
-            star
-        ):
-        score = ScoreModel()
-
-        score.userid = userid
-        score.movieid = movieid
-        score.comDate = comDate
-        score.value = value
-        score.content = content
-        score.watched = watched
-        score.star = star
-
-        score.save()
-        return score
 
     def test_instance(self):
         pass
@@ -81,54 +135,6 @@ class TestScoreModel(unittest.TestCase):
 
 
 class TestMovieModel(unittest.TestCase):
-
-    def create_movie_instance(
-            movieid,
-            mname,
-            director,
-            editor,
-            actor,
-            mtype,
-            countryOrLocation,
-            language,
-            date,
-            duration,
-            othername,
-            IMDb,
-            introduction,
-            allActors,
-            numOfEvaluator,
-            star5,
-            star4,
-            star3,
-            star2,
-            star1
-        ):
-        movie = MovieModel()
-
-        movie.movieid = movieid
-        movie.mname = mname
-        movie.director = director
-        movie.editor = editor
-        movie.actor = actor
-        movie.mtype = mtype
-        movie.countryOrLocation = countryOrLocation
-        movie.language = language
-        movie.date = date
-        movie.duration = duration
-        movie.othername = othername
-        movie.IMDb = IMDb
-        movie.introduction = introduction
-        movie.allActors = allActors
-        movie.numOfEvaluator = numOfEvaluator
-        movie.star5 = star5
-        movie.star4 = star4
-        movie.star3 = star3
-        movie.star2 = star2
-        movie.star1 = star1
-
-        movie.save()
-        return movie
 
     def test_instance(self):
         pass
