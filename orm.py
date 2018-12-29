d20a63a8730cccb9f8f46afb2c2bea2fcd007d3d
mu001999 <mu001999@outlook.com>
2018-12-29 17:03:15 +0800

    rename dao.py to orm.py

---
+++ orm.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+import MySQLdb as mdb
+
+
+class Field(object):
+    pass
+
+
+class Expr(object):
+    def __init__(self, model, kwargs):
+        self.model = model
+        self.params = kwargs.values()
+        equations = [key + ' = %s' for key in kwargs.keys()]
+        self.where_expr = 'where ' + ' and '.join(equations) if len(equations) > 0 else ''
+
+    def update(self, **kwargs):
+        keys = []
+        params = []
+        for key, val in kwargs.iteritems():
+            if val is None or key not in self.model.fields:
+                continue
+            keys.append(key)
+            params.append(val)
+        params.extern(slef.params)
+        sql = 'update %s set %s %s;' % (
+            self.model.table,
+            ', '.join([key + ' = %s' for key in keys]),
+            self.where_expr
+        )
+        return Database.execute(sql, params)
+
+    def limit(self, rows, offset=None):
+        self.where_expr += 'limit %s%s' % (
+            '%s, ' % offset if offset is not None else '',
+            rows
+        )
+        return self
+
+    def select(self):
+        sql = 'select %s from %s %s;' % (
+            ', '.join(slef.model.fields.keys()),
+            self.model.table,
+            self.where_expr
+        )
+        for row in Database.execute(sql, self.params).fetchall():
+            instance = self.model()
+            for index, val in enumerate(row):
+                setattr(instance, self.model.fields.keys()[index], val)
+            yield instance
+
+    def count(self):
+        sql = 'select count(*) from %s %s;' % (
+            self.model.table,
+            self.where_expr
+        )
+        cnt, = Database.execute(sql, self.params).fetchone()
+        return cnt
+
+
+class ModelMetaclass(type):
+    table = None
+    fields = dict()
+
+    def __init__(self, name, bases, attrs):
+        super(ModelMetaclass, self).__init__(name, bases, attrs)
+        for key, val in self.__dict__.iteritems():
+            if isinstance(val, Field):
+                self.fields[key] = val
+
+
+class Model(object):
+    __metaclass__ = ModelMetaclass
+
+    def save(self):
+        sql = 'insert ignore into %s(%s) values (%s);' % (
+            self.table,
+            ', '.join(self.__dict__.keys()),
+            ', '.join(['%s'] * len(self.__dict__))
+        )
+        return Database.execute(sql, self.__dict__.values())
+
+    @classmethod
+    def where(cls, **kwargs):
+        return Expr(cls, kwargs)
+
+
+class Database(object):
+    autocommit = True
+    connection = None
+    db_config = dict()
+
+    @classmethod
+    def connect(cls, **db_config):
+        cls.connection = mdb.connect(
+            host=db_config.get('host', 'localhost'),
+            port=int(db_config.get('port', 2222)),
+            user=db_config.get('user', 'root'),
+            passwd=db_config.get('password', ''),
+            db=db_config.get('database', 'test'),
+            charset=db_config.get('charset', 'utf-8')
+        )
+        cls.connection.autocommit(cls.autocommit)
+        cls.db_config.update(db_config)
+
+    @classmethod
+    def get_connection(cls):
+        if not cls.connection or not cls.connection.open:
+            cls.connect(**cls.db_config)
+        try:
+            cls.connection.ping()
+        except mdb.OperationalError:
+            cls.connect(**cls.db_config)
+        return cls.connection
+
+    @classmethod
+    def execute(cls, *args):
+        cursor = cls.get_connection().cursor()
+        cursor.execute(*args)
+        return cursor
+
+    def __del__(self):
+        if self.connection and self.conn.open:
+            self.connection.close()
+
+
+def execute_raw_sql(sql, params=None):
+    return Database.execute(sql, params) if params else Database.execute(sql)
