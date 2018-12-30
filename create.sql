bf758c472f9a58ead2796877af30035704e13eda
mu001999 <mu001999@outlook.com>
2018-12-30 14:52:37 +0800

    init create.sql

---
+++ create.sql
@@ -0,0 +1,124 @@
+-- MySQL dump 10.16  Distrib 10.1.37-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: dodo
+-- ------------------------------------------------------
+-- Server version	10.1.37-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Movie`
+--
+
+DROP TABLE IF EXISTS `Movie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Movie` (
+  `movieid` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `director` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `editor` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `actor` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mtype` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `countryOrLocation` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `language` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `duration` int(11) NOT NULL,
+  `othername` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `IMDb` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `introduction` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `allActors` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `numOfEvaluator` int(11) NOT NULL,
+  `star5` int(11) NOT NULL,
+  `star4` int(11) NOT NULL,
+  `star3` int(11) NOT NULL,
+  `star2` int(11) NOT NULL,
+  `star1` int(11) NOT NULL,
+  PRIMARY KEY (`movieid`),
+  UNIQUE KEY `AK_MOVIE_MOVIE` (`movieid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Movie`
+--
+
+LOCK TABLES `Movie` WRITE;
+/*!40000 ALTER TABLE `Movie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Movie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Score`
+--
+
+DROP TABLE IF EXISTS `Score`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Score` (
+  `userid` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `movieid` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Mov_movieid` char(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Use_userid` char(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `comDate` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` int(11) NOT NULL,
+  `content` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `watched` smallint(6) NOT NULL,
+  `star` int(11) NOT NULL,
+  PRIMARY KEY (`movieid`,`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Score`
+--
+
+LOCK TABLES `Score` WRITE;
+/*!40000 ALTER TABLE `Score` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Score` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `userid` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `record` varchar(300) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-12-30 14:45:55
