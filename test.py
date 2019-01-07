133dc2248d686600fc0f93b37fd2f1addc337602
mu001999 <mu001999@outlook.com>
2019-01-07 21:19:42 +0800

    update test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from orm import execute_raw_sql
-from model import UserModel, ScoreModel, MovieModel
+from model import *
 import unittest
 
 
@@ -94,6 +94,121 @@ def create_movie_instance(
     movie.save()
     return movie
 
+def create_editor_instance(
+        ediid,
+        ediname,
+        edisex,
+        ediage,
+        edicountry,
+        ediheight,
+        edibirthplace,
+        edination,
+        ediconstellation
+    ):
+    editor = EditorModel()
+
+    editor.ediid = ediid
+    editor.ediname = ediname
+    editor.edisex = edisex
+    editor.ediage = ediage
+    editor.edicountry = edicountry
+    editor.ediheight = ediheight
+    editor.edibirthplace = edibirthplace
+    editor.edination = edination
+    editor.ediconstellation = ediconstellation
+
+    editor.save()
+    return editor
+
+def create_actor_instance(
+        actorid,
+        actorname,
+        sex,
+        age,
+        country,
+        height,
+        birthplace,
+        nation,
+        constellation
+    ):
+    actor = ActorModel()
+
+    actor.actorid = actorid
+    actor.actorname = actorname
+    actor.sex = sex
+    actor.age = age
+    actor.country = country
+    actor.height = height
+    actor.birthplace = birthplace
+    actor.nation = nation
+    actor.constellation = constellation
+
+    actor.save()
+    return actor
+
+def create_director_instance(
+        dirtorid,
+        dirorname,
+        dirsex,
+        dirage,
+        dircountry,
+        dirheight,
+        dirbirthplace,
+        dirnation,
+        dirconstellation
+    ):
+    director = DirectorModel()
+
+    director.dirtorid = dirtorid
+    director.dirorname = dirorname
+    director.dirsex = dirsex
+    director.dirage = dirage
+    director.dircountry = dircountry
+    director.dirheight = dirheight
+    director.dirbirthplace = dirbirthplace
+    director.dirnation = dirnation
+    director.dirconstellation = dirconstellation
+
+    director.save()
+    return director
+
+def create_movact_instance(
+        movieid,
+        actorid,
+        character
+    ):
+    movact = MovActModel()
+
+    movact.movieid = movieid
+    movact.actorid = actorid
+    movact.character = character
+
+    movact.save()
+    return movact
+
+def create_md_instance(
+        movieid,
+        dirtorid
+    ):
+    md = MDModel()
+
+    md.movieid = movieid
+    md.dirtorid = dirtorid
+
+    md.save()
+    return md
+
+def create_me_instance(
+        movieid,
+        ediid
+    ):
+    me = MEModel()
+
+    me.movieid = movieid
+    me.ediid = ediid
+
+    me.save()
+    return me
 
 class TestUserModel(unittest.TestCase):
 
@@ -123,10 +238,10 @@ class TestUserModel(unittest.TestCase):
                 0
             )
 
-    def tearDown(self):
+    def rm_tearDown(self):
         execute_raw_sql("truncate table User;")
 
-    def test_instance(self):
+    def test_main(self):
         user = UserModel.where(userid="1998080911").select().next()
 
         self.assertEquals(user.userid, "1998080911")
@@ -134,19 +249,19 @@ class TestUserModel(unittest.TestCase):
         self.assertEquals(user.password, "Leo980809")
         self.assertEquals(user.record, u"来电狂想/云南虫谷/无敌破坏王")
 
-    def test_select(self):
+
         users = UserModel.where().select()
 
         for user in users:
-            if user.userid == "1998080911": pass
+            if user.userid == "1999080611": pass
             elif user.userid == "1999022511": pass
             else:
-                self.assertEquals(user.userid, "1999080611")
-                self.assertEquals(user.username, "likemilk")
-                self.assertEquals(user.password, "woaihemilk")
-                self.assertEquals(user.record, u"龙猫/狗十三")
+                self.assertEquals(user.userid, "1998080911")
+                self.assertEquals(user.username, "strawberry")
+                self.assertEquals(user.password, "Leo980809")
+                self.assertEquals(user.record, u"来电狂想/云南虫谷/无敌破坏王")
 
-    def test_update(self):
+
         UserModel.where(userid="1998080911").update(record=u"来电狂想/云南虫谷/无敌破坏王")
         user = UserModel.where(userid="1998080911").select().next()
 
@@ -155,14 +270,14 @@ class TestUserModel(unittest.TestCase):
         self.assertEquals(user.password, "Leo980809")
         self.assertEquals(user.record, u"来电狂想/云南虫谷/无敌破坏王")
 
-    def test_limit(self):
+
         users = UserModel.where(username="strawberry").limit(2).select()
 
         cnt = 0
         for user in users: cnt += 1
         self.assertEquals(1, cnt)
 
-    def test_count(self):
+
         cnt = UserModel.where(username="strawberry").count()
 
         self.assertEquals(1, cnt)
@@ -191,7 +306,7 @@ class TestScoreModel(unittest.TestCase):
                 1,
                 3
             )
-        if list(UserModel.where(userid="1999080611").select()) == []:
+        if list(ScoreModel.where(userid="1999080611").select()) == []:
             create_score_instance(
                 "1999080611",
                 "0000000003",
@@ -202,10 +317,10 @@ class TestScoreModel(unittest.TestCase):
                 5
             )
 
-    def tearDown(self):
+    def rm_tearDown(self):
         execute_raw_sql("truncate table Score;")
 
-    def test_instance(self):
+    def test_main(self):
         score = ScoreModel.where(userid="1998080911").select().next()
 
         self.assertEquals(score.userid, "1998080911")
@@ -216,7 +331,7 @@ class TestScoreModel(unittest.TestCase):
         self.assertEquals(score.watched, 1)
         self.assertEquals(score.star, 4)
 
-    def test_select(self):
+
         scores = ScoreModel.where().select()
 
         for score in scores:
@@ -232,7 +347,7 @@ class TestScoreModel(unittest.TestCase):
                 self.assertEquals(score.star, 5)
 
 
-    def test_update(self):
+
         ScoreModel.where(userid="1999080611").update(star=2)
         score = ScoreModel.where(userid="1999080611").select().next()
 
@@ -244,14 +359,14 @@ class TestScoreModel(unittest.TestCase):
         self.assertEquals(score.watched, 1)
         self.assertEquals(score.star, 2)
 
-    def test_limit(self):
+
         scores = ScoreModel.where(content=u"电影肥肠好看哈哈哈哈哈哈").limit(2).select()
 
         cnt = 0
         for score in scores: cnt += 1
         self.assertEquals(1, cnt)
 
-    def test_count(self):
+
         cnt = ScoreModel.where(content=u"电影肥肠好看哈哈哈哈哈哈").count()
 
         self.assertEquals(1, cnt)
@@ -260,13 +375,13 @@ class TestScoreModel(unittest.TestCase):
 class TestMovieModel(unittest.TestCase):
 
     def setUp(self):
-        if list(MovieModel.where(movieid="9999999991").select()) == []:
+        if list(MovieModel.where(movieid="0000000001").select()) == []:
             create_movie_instance(
-                "9999999991",
+                "0000000001",
                 u"海王",
                 u"温子仁",
                 u"编剧",
-                u"罗钥轩/海王",
+                u"师毓洁/智障",
                 u"玄幻",
                 u"中国",
                 u"中文",
@@ -275,7 +390,7 @@ class TestMovieModel(unittest.TestCase):
                 u"海的女儿",
                 u"www.123.com",
                 u"很好看的电影",
-                u"师毓洁/智障",
+                u"罗钥轩/海王",
                 3466,
                 1531,
                 245,
@@ -283,9 +398,9 @@ class TestMovieModel(unittest.TestCase):
                 1000,
                 234
             )
-        if list(MovieModel.where(movieid="9999999992").select()) == []:
+        if list(MovieModel.where(movieid="0000000002").select()) == []:
             create_movie_instance(
-                "9999999992",
+                "0000000002",
                 u"龙猫",
                 u"导演2",
                 u"编剧2",
@@ -306,9 +421,9 @@ class TestMovieModel(unittest.TestCase):
                 1300,
                 1300
             )
-        if list(MovieModel.where(movieid="9999999993").select()) == []:
+        if list(MovieModel.where(movieid="0000000003").select()) == []:
             create_movie_instance(
-                "9999999993",
+                "0000000003",
                 u"狗十三",
                 u"曹保平",
                 u"编剧3",
@@ -330,43 +445,338 @@ class TestMovieModel(unittest.TestCase):
                 100
             )
 
-    def tearDown(self):
+    def rm_tearDown(self):
         execute_raw_sql("truncate table Movie;")
 
-    def test_instance(self):
-        movie = MovieModel.where(movieid="9999999991").select().next()
+    def test_main(self):
+        movie = MovieModel.where(movieid="0000000001").select().next()
 
-        self.assertEquals(movie.movieid, "9999999991")
+        self.assertEquals(movie.movieid, "0000000001")
         self.assertEquals(movie.mname, u"海王")
 
-    def test_select(self):
+
         movies = MovieModel.where().select()
 
         for movie in movies:
-            if movie.movieid == "9999999991": pass
-            elif movie.movieid == "9999999992": pass
+            if movie.movieid == "0000000001": pass
+            elif movie.movieid == "0000000002": pass
             else:
-                self.assertEquals(movie.movieid, "9999999993")
+                self.assertEquals(movie.movieid, "0000000003")
                 self.assertEquals(movie.mname, u"狗十三")
 
-    def test_update(self):
-        MovieModel.where(movieid="9999999991").update(mname=u"海的女儿")
-        movie = MovieModel.where(movieid="9999999991").select().next()
+
+        MovieModel.where(movieid="0000000001").update(mname=u"海的女儿")
+        movie = MovieModel.where(movieid="0000000001").select().next()
 
         self.assertEquals(movie.mname, u"海的女儿")
 
-    def test_limit(self):
+
         movies = MovieModel.where(introduction=u"很好看的电影").limit(2).select()
 
         cnt = 0
         for movie in movies: cnt += 1
         self.assertEquals(1, cnt)
 
-    def test_count(self):
+
         cnt = MovieModel.where(introduction=u"很好看的电影").count()
 
         self.assertEquals(1, cnt)
 
+class TestEditorModel(unittest.TestCase):
+    
+    def setUp(self):
+        if list(EditorModel.where(ediid="1111100001").select()) == []:
+            create_editor_instance(
+                "1111100001",
+                u"编剧",
+                u"男",
+                56,
+                u"中国",
+                178,
+                u"沈阳市沈河区",
+                u"汉族",
+                u"处女座"
+            )
+       
+    def rm_tearDown(self):
+        execute_raw_sql("truncate table Editor;")
+
+    def test_main(self):
+        editor = EditorModel.where(ediid="1111100001").select().next()
+
+        self.assertEquals(editor.ediname, u"编剧")
+
+
+        editors = EditorModel.where().select()
+
+        for editor in editors:
+            if editor.ediid == "1111100002": pass
+            elif editor.ediid == "1111100003": pass
+            else:
+                self.assertEquals(editor.ediname, u"编剧")
+
+
+        EditorModel.where(ediid="1111100001").update(ediname=u"罗钥轩")
+        editor = EditorModel.where(ediid="1111100001").select().next()
+
+        self.assertEquals(editor.ediid, "1111100001")
+        self.assertEquals(editor.ediname, u"罗钥轩")
+
+
+        editors = EditorModel.where(edicountry=u"中国").limit(2).select()
+
+        cnt = 0
+        for editor in editors: cnt += 1
+        self.assertEquals(1, cnt)
+
+
+        cnt = EditorModel.where(edicountry=u"中国").count()
+
+        self.assertEquals(1, cnt)
+        
+class TestActorModel(unittest.TestCase):
+
+    def setUp(self):
+        if list(ActorModel.where(actorid="2222200001").select()) == []:
+            create_actor_instance(
+                "2222200001",
+                u"师毓洁",
+                u"女",
+                19,
+                u"中国",
+                169,
+                u"陕西省",
+                u"汉族",
+                u"双子座"
+            )
+
+    def rm_tearDown(self):
+        execute_raw_sql("truncate table Actor;")
+
+    def test_main(self):
+        actor = ActorModel.where(actorid="2222200001").select().next()
+
+        self.assertEquals(actor.actorname, u"师毓洁")
+
+
+        actors = ActorModel.where().select()
+
+        for actor in actors:
+            if actor.actorid == "2222200002": pass
+            elif actor.actorid == "2222200003": pass
+            else:
+                self.assertEquals(actor.actorname, u"师毓洁")
+
+
+        ActorModel.where(actorid="2222200001").update(actorname=u"罗钥轩")
+        actor = ActorModel.where(actorid="2222200001").select().next()
+
+        self.assertEquals(actor.actorid, "2222200001")
+        self.assertEquals(actor.actorname, u"罗钥轩")
+
+
+        actors = ActorModel.where(constellation=u"双子座").limit(2).select()
+
+        cnt = 0
+        for actor in actors: cnt += 1
+        self.assertEquals(1, cnt)
+
+
+        cnt = ActorModel.where(constellation=u"双子座").count()
+
+        self.assertEquals(1, cnt)
+
+class TestDirectorModel(unittest.TestCase):
+    
+    def setUp(self):
+        if list(DirectorModel.where(dirtorid="3333300001").select()) == []:
+            create_director_instance(
+                "3333300001",
+                u"温子仁",
+                u"男",
+                42,
+                u"马来西亚",
+                168,
+                u"马来西亚-砂拉越州-古晋",
+                u"汉族",
+                u"双鱼座"
+            )
+       
+    def rm_tearDown(self):
+        execute_raw_sql("truncate table Director;")
+
+    def test_main(self):
+        director = DirectorModel.where(dirtorid="3333300001").select().next()
+
+        self.assertEquals(director.dirorname, u"温子仁")
+
+
+        directors = DirectorModel.where().select()
+
+        for director in directors:
+            if director.dirtorid == "3333300002": pass
+            elif director.dirtorid == "3333300003": pass
+            else:
+                self.assertEquals(director.dirorname, u"温子仁")
+
+
+        DirectorModel.where(dirtorid="3333300001").update(dirorname=u"罗钥轩")
+        director = DirectorModel.where(dirtorid="3333300001").select().next()
+
+        self.assertEquals(director.dirtorid, "3333300001")
+        self.assertEquals(director.dirorname, u"罗钥轩")
+
+
+        directors = DirectorModel.where(dirage=42).select()
+
+        cnt = len(list(directors))
+        self.assertEquals(1, cnt)
+
+
+        cnt = DirectorModel.where(dirage=42).count()
+
+        self.assertEquals(1, cnt)
+"""
+class TestMovActModel(unittest.TestCase):
+    
+    def setUp(self):
+        if list(MovActModel.where(movieid="0000000001").select()) == []:
+            create_movact_instance(
+                "0000000001",
+                "2222200001",
+                 u"智障"
+            )
+       
+    def rm_tearDown(self):
+        execute_raw_sql("truncate table MovAct;")
+
+    def test_main(self):
+        movact = MovActModel.where(actorid="2222200001").select().next()
+
+        self.assertEquals(movact.character, u"智障")
+
+ 
+        movacts = MovActModel.where().select()
+
+        for movact in movacts:
+            if movact.actorid == "2222200002": pass
+            elif movact.actorid == "2222200003": pass
+            else:
+                self.assertEquals(movact.character, u"智障")
+
+
+        MovActModel.where(actorid="2222200001").update(character=u"海王")
+        movacts = MovActModel.where(actorid="2222200001").select().next()
+
+        self.assertEquals(movact.actorid, "2222200001")
+        self.assertEquals(movact.character, u"海王")
+
+
+        movacts = MovActModel.where(character=u"智障").limit(2).select()
+
+        cnt = 0
+        for movact in movacts: cnt += 1
+        self.assertEquals(1, cnt)
+
+
+        cnt = MovActModel.where(character=u"智障").count()
+
+        self.assertEquals(1, cnt)
+"""
+
+class TestMEModel(unittest.TestCase):
+    
+    def setUp(self):
+        if list(MEModel.where(movieid="0000000001").select()) == []:
+            create_me_instance(
+                "0000000001",
+                "1111100001"
+            )
+       
+    def rm_tearDown(self):
+        execute_raw_sql("truncate table ME;")
+
+    def test_main(self):
+        me = MEModel.where(movieid="0000000001").select().next()
+
+        self.assertEquals(me.ediid, "1111100001")
+
+    
+        mes = MEModel.where().select()
+
+        for me in mes:
+            if me.movieid == "0000000002": pass
+            elif me.movieid == "0000000003": pass
+            else:
+                self.assertEquals(me.ediid, "1111100001")
+
+        """
+        MEModel.where(movieid="0000000001").update(ediid="1111100011")
+        mes = MEModel.where(movieid="0000000001").select().next()
+
+        self.assertEquals(me.movieid, "0000000001")
+        self.assertEquals(me.ediid, "1111100001")
+        """
+
+        mes = MEModel.where(movieid="0000000001").limit(2).select()
+
+        cnt = 0
+        for me in mes: cnt += 1
+        self.assertEquals(1, cnt)
+
+
+        cnt = MEModel.where(movieid="0000000001").count()
+
+        self.assertEquals(1, cnt)
+
+class TestMDModel(unittest.TestCase):
+    
+    def setUp(self):
+        if list(MDModel.where(movieid="0000000001").select()) == []:
+            create_md_instance(
+                "0000000001",
+                "3333300001"
+            )
+       
+    def rm_tearDown(self):
+        execute_raw_sql("truncate table MD;")
+
+    def test_main(self):
+        md = MDModel.where(movieid="0000000001").select().next()
+
+        self.assertEquals(md.dirtorid, "3333300001")
+
+   
+        mds = MDModel.where().select()
+
+        for md in mds:
+            if md.movieid == "0000000002": pass
+            elif md.movieid == "0000000003": pass
+            else:
+                self.assertEquals(md.dirtorid, "3333300001")
+
+        """
+        MDModel.where(movieid="0000000001").update(dirtorid="1111100011")
+        mds = MDModel.where(movieid="0000000001").select().next()
+
+        self.assertEquals(md.movieid, "0000000001")
+        self.assertEquals(md.dirtorid, "3333300001")
+        """
+   
+        mds = MDModel.where(movieid="0000000001").limit(2).select()
+
+        cnt = 0
+        for md in mds: cnt += 1
+        self.assertEquals(1, cnt)
+
+    
+        cnt = MDModel.where(movieid="0000000001").count()
+
+        self.assertEquals(1, cnt)
+
+
 
 if __name__ == "__main__":
+    for table in ['User', 'Movie', 'Score', 'Editor', 'Director', 'Actor']:
+        execute_raw_sql("truncate table " + table + ";")
     unittest.main()
